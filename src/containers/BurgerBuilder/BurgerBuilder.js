9a02a760972770eab982cccb0e3500e8f22906c8
PeterLynchJ <jiaochenwei@gmail.com>
2019-03-19 23:49:53 -0400

    initial commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aux from '../../hoc/Auxi'
+
+class BurgerBuilder extends React.Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Burger Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
