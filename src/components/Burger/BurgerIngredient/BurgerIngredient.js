8ef5bd3527c3453a56b1b4d7f59d93e201c5210f
Peter Jiao <peter.jiao.msi@gmail.com>
2019-04-03 18:04:54 -0400

    burger

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import classes from './BurgerIngredient.css';
+import PropTypes from 'prop-types';
+
+const BurgerIngredient = props => {
+  let ingredient = null;
+  switch(props.type) {
+    case 'bread-bottom':
+      ingredient = <div className={classes['bread-bottom']}></div>; break;
+    case 'bread-top':
+      ingredient =
+        <div className={classes['bread-top']}>
+          <div className={classes['seeds']}></div>
+          <div className={classes['seeds2']}></div>
+        </div>; break;
+    case 'meat':
+      ingredient = <div className={classes['meat']}></div>; break;
+    case 'cheese':
+      ingredient = <div className={classes['cheese']}></div>; break;
+    case 'bacon':
+      ingredient = <div className={classes['bacon']}></div>; break;
+    case 'salad':
+      ingredient = <div className={classes['salad']}></div>; break;
+    default:
+      ingredient = null; break;
+  }
+  return ingredient;
+};
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+};
+
+export default BurgerIngredient;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Auxi'
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends React.Component {
     render() {
         return (
             <Aux>
-                <div>Burger</div>
-                <div>Burger Controls</div>
+                <Burger />
+                <div>Build Controls</div>
             </Aux>
         );
     }
